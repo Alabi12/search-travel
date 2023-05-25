425f0b4c82abfdba7c5164825c698aed9f067b19
Alabi12 <wolo22001@gmail.com>
2023-05-25 22:57:25 +0000

    Install next.js file and dependencies

--- src/pages/ListingPage.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const ListingPage = () => {
-  return <h1>Listing Page</h1>;
-};
-
-export default ListingPage;
--- src/pages/SearchResultsPage.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const SearchResultsPage = () => {
-  return <h1>Search Results Page</h1>;
-};
-
-export default SearchResultsPage;
